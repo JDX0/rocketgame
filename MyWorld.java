a43f048210734d3e334146dc632d54caf31019ed
JDX <jdx@tutanota.de>
2021-02-10 19:36:32 +0100

    increased resolution

--- MyWorld.java
+++ MyWorld.java
@@ -17,7 +17,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x800 cells with a cell size of 1x1 pixels.
-        super(800, 700, 1); 
+        super(1366, 768, 1); 
         prepare();
         
     }
@@ -28,9 +28,8 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-        rocket = new Rocket(2,1);
-        addObject(rocket,272,542);
-        rocket.setLocation(406,758);
+        rocket = new Rocket(5,1);
+        addObject(rocket,653,542);
         Asteroid asteroid = new Asteroid(4);
         addObject(asteroid,81,53);
         Asteroid asteroid2 = new Asteroid(3);
@@ -44,7 +43,9 @@ public class MyWorld extends World
         data = new InfoPanel();
         addObject(data, 400, 10);
         data.write("Zde jsou data o raketě");
-        rocket.setLocation(396,659);
+        Planet planet = new Planet(1);
+        addObject(planet,958,162);
+        planet.setLocation(980,13);
     }
     
     public void act()
--- TraderWorld.java
+++ TraderWorld.java
@@ -17,7 +17,7 @@ public class TraderWorld extends World
     public TraderWorld(World w, Rocket r)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(1366, 768, 1);
         this.previous = w;
         this.myRocket = r;
         this.myRocket.getTank().refuel(100);
