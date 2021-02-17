f4745c33575a9f1f2d81d5ad6fe42d2f9e5f0345
JDX <jdx@tutanota.de>
2021-02-17 09:54:24 +0100

    new class

---
+++ Explosion.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Explosion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Explosion extends Actor
+{
+    private int length;
+    public Explosion(int length){
+        this.length = length;
+    }
+    public void act() 
+    {
+        Greenfoot.delay(10);
+        this.getWorld().removeObject(this);
+    }    
+}
